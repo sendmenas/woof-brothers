4ad581cf6a100b4b64f5e622bdbbecb4da2f2d6f
Giedrius Bielskis <Giedrius Bielskis>
2016-10-21 22:06:09 +0300

    various updates

--- js/script.js
+++ js/script.js
@@ -1,35 +1,45 @@
 $(document).ready(function() {
 
+	$("#derekChar").dialog({ autoOpen: false });
+	$("#delChar").dialog({ autoOpen: false });
+	
 	/* Character selection */
 	$(".brother").click(function() {
-
 		var firstSelection = confirm("Are you sure you want to choose " + brotherSelect + "?");
 		if (firstSelection == true) {
 
 			var brotherSelect = $(this).attr("id");
 	
 			if (brotherSelect === "Derek") {
+				$("#derekChar").dialog("open");
+				$("#derekChar").dialog({width: 500}); // http://api.jqueryui.com/dialog/		
+				$(".ui-button").click(function() {
+					$(this).parents("body").children(".gameStory").first().toggle();
+					$(this).parents("body").children(".gameStory").first().find(".comment").toggle(1000);
+					$("html, body").animate({scrollTop: $(this).parents("body").children(".gameStory").first().offset().top}, 500);
+				});
 				$(".thumbnail").removeClass("selected");
 				$(this).addClass("selected");
 				$(this).parents().next().css("display", "none");
 				$(this).parent().removeClass("col-md-3 col-md-offset-3").addClass("col-md-4 col-md-offset-4");
 			}
 			if (brotherSelect === "Del") {
+				$("#delChar").dialog("open");
+				$(".ui-button").click(function() {
+					$(this).parents("body").children(".gameStory").first().toggle();
+					$(this).parents("body").children(".gameStory").first().find(".comment").toggle(1000);
+					$("html, body").animate({scrollTop: $(this).parents("body").children(".gameStory").first().offset().top}, 500);
+				});
 				$(".thumbnail").removeClass("selected");
 				$(this).addClass("selected");
 				$(this).parents().prev().css("display", "none");
 				$(this).parent().removeClass("col-md-3 col-md-offset-3").addClass("col-md-4 col-md-offset-4");
 			}
-			$(this).parents(".main").next().toggle();
-			$(this).parents(".main").next().find(".comment").toggle(1000);
-			$("html, body").animate({scrollTop: $(this).parents(".main").next().offset().top}, 500);
 			$(".brother").off();
 		}
 		else {
 				$(".thumbnail").removeClass("selected");
 		}
-		
-		
 
 		$(".continue").click(function() {
 			if (brotherSelect === "Derek") {
@@ -43,14 +53,11 @@ $(document).ready(function() {
 				$(this).parents(".gameStory").nextAll(".del").first().find(".comment").toggle(1000);
 			}
 		});
-
-
 	});
 
 	/* Action buttons */
 	$(".yesNo").click(function() {
 		var banditSelect = $(this).html();
-		console.log(banditSelect);
 
 		if (banditSelect === "Yes") {
 			alert("Please reconsider");
